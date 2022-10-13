628a3e581a68e1855e22d22cc56fcbce2b7fee85
shaahin <shahin.developer7@gmail.com>
2022-10-13 19:19:40 +0330

    add comment and post model

---
+++ app/Models/Comment.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "parent_id",
+        "name",
+        "comment",
+    ];
+
+    public function replies(): HasMany
+    {
+        return $this->hasMany(Comment::class, 'parent_id');
+    }
+
+    public function commentable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+}
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,7 +13,6 @@ class Post extends Model
     protected $fillable = [
         "title",
         "description",
-        "visit",
     ];
 
     public function comments(): MorphMany
